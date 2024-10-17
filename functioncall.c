9e5b5cc6b5323f4fc010cbadd6bbc9979659facf
Rigo74215 <rigobrazo67@gmail.com>
2024-10-17 11:56:45 +0300

    Update functioncall.c

--- functioncall.c
+++ functioncall.c
@@ -1,31 +1,74 @@
-//function to call electric bill
+//program for function call
 /*
-Author: Reagan galo
-Date: 10/12/2024
-Reg no: Ct101/g/23572/24
+Author: Reagan Galo
+Date: 10/17/2024
+Reg no: CT101/G/23572/24
 */
-#include <stdio.h>
 
-// Function to calculate electric bill
-	float calculateElectricBill
-   (float kWh) {
-    float rate = 0.50;
-    float bill = kWh * rate;
+#include <stdio.h>
+#include <string.h>
+#include <math.h>  
+
+float calculateElectricBill(int units) {
+    float rate, bill;
+    if (units <= 199) {
+        rate = 1.20;
+    } else if (units >= 200 && units < 400) {
+        rate = 1.50;
+    } else if (units >= 400 && units < 600) {
+        rate = 1.80;
+    } else {
+        rate = 2.00;
+    }
+
+    bill = units * rate;
+    
+    if (bill > 400) {
+        bill += bill * 0.15; 
+    }
+
+    if (bill < 100) {
+        bill = 100;
+    }
+
     return bill;
 }
 
 int main() {
-    float kWh, bill;
+    int customerID, unitsConsumed;
+    char customerName[50], copiedCustomerName[50];  
+    float bill, rate;
 
-    // Input from the user
-    printf("Enter the number of kilowatt-hours used: ");
-    scanf("%f", &kWh);
+    printf("Enter Customer ID: ");
+    scanf("%d", &customerID);
+    printf("Enter Customer Name: ");
+    scanf("%s", customerName);  
+    printf("Enter Units Consumed: ");
+    scanf("%d", &unitsConsumed);
 
-    // Calculate the bill using the function
-    bill = calculateElectricBill(kWh); // Function call
+    strcpy(copiedCustomerName, customerName); 
+   
+    bill = calculateElectricBill(unitsConsumed);
+    
+    bill = ceil(bill);
 
-    // Output the result
-    printf("The total electric bill is: ksh %.2f\n", bill);
+    printf("\n   Electricity Bill \n");
+    printf("Customer ID: %d\n", customerID);
+    printf("Customer Name: %s\n", copiedCustomerName); 
+    printf("Units Consumed: %d\n", unitsConsumed);
+    
+    if (unitsConsumed <= 199) {
+        rate = 1.20;
+    } else if (unitsConsumed >= 200 && unitsConsumed < 400) {
+        rate = 1.50;
+    } else if (unitsConsumed >= 400 && unitsConsumed < 600) {
+        rate = 1.80;
+    } else {
+        rate = 2.00;
+    }
+
+    printf("Charge per Unit: %.2f\n", rate);
+    printf("Total Amount to Pay: Ksh %.2f\n", bill);
 
     return 0;
 }
